bd5961180bdcd81c7fbe7b414e8bfb129f4bb49c
shokurayo <113115947+shokurayo@users.noreply.github.com>
2022-10-07 01:25:48 +0300

    Create against2.cpp

---
+++ against2.cpp
@@ -0,0 +1,21 @@
+while (hero.get_hp() > 0 && water_slime.get_status())
+        {
+            cout << "YOU ARE AGAINST WATER SLIME, PREPARE YOURSELF" << endl;
+
+            cout << "Choose element: ";
+            cin >> our_color;
+            sword.set_color(our_color);
+
+            if (water_slime.get_color() == sword.get_color())
+            {
+                water_slime.set_status(false);
+                cout << "Congratulations! you have dfeated this slime" << endl;
+            }
+            else
+            {
+                int taken_damage = water_slime.splash();
+                hero.set_hp(hero.get_hp() - taken_damage);
+                cout << "You've taken: " << taken_damage << endl;
+                cout << "HP: " << hero.get_hp() << endl;
+            }
+        }
