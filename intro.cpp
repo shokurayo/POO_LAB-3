ac795e2a97c234d89a818e3fc7ad0e2436887016
shokurayo <113115947+shokurayo@users.noreply.github.com>
2022-10-07 01:24:38 +0300

    Create intro.cpp

---
+++ intro.cpp
@@ -0,0 +1,7 @@
+string our_color;
+
+    cout << "YOU ARE A HERO THAT HAS TO DEFEAT 7 SLIMES" << endl;
+    cout << "CHOOSE THE RIGHT ELEMENT TO DEFEAT A SLIME" << endl;
+    cout << "air, fire, earth, water, dark, light, god" << endl;
+    cout << "Your HP: " << hero.get_hp() << endl;
+    cout << endl;
