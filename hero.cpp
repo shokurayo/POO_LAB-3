a64bb3a86677e532c79c4bba56140edac8900bf4
shokurayo <113115947+shokurayo@users.noreply.github.com>
2022-10-07 01:19:22 +0300

    Create hero.cpp

---
+++ hero.cpp
@@ -0,0 +1,15 @@
+class Hero
+{
+    int hp;
+
+    public:
+        void set_hp(int the_hp)
+        {
+            hp = the_hp;
+        }
+
+        int get_hp()
+        {
+            return hp;
+        }
+};
