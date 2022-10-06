df4c7d15d7ce91640714e01cfe8e285e53cfff80
shokurayo <113115947+shokurayo@users.noreply.github.com>
2022-10-07 01:20:07 +0300

    Create sword.cpp

---
+++ sword.cpp
@@ -0,0 +1,16 @@
+class Sword
+{
+    protected:
+        string color;
+    
+    public:
+        void set_color(string the_color)
+        {
+            color = the_color;
+        }
+        string get_color()
+        {
+            return color;
+        }
+
+};
