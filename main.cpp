c82afee6ce6b96ef24d2ef3f8b1e7fa93ec0f8a1
shokurayo <113115947+shokurayo@users.noreply.github.com>
2022-10-07 01:24:08 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+
+int main(void)
+{
+    Hero hero;
+    hero.set_hp(100);
+    Sword sword;
+
+    AirSlime air_slime;
+    air_slime.set_air_slime();
+    FireSlime fire_slime;
+    fire_slime.set_fire_slime();
+    WaterSlime water_slime;
+    water_slime.set_water_slime();
+    EarthSlime earth_slime;
+    earth_slime.set_earth_slime();
+    DarkSlime dark_slime;
+    dark_slime.set_dark_slime();
+    LightSlime light_slime;
+    light_slime.set_light_slime();
+    GodSlime god_slime;
+    god_slime.set_god_slime();
